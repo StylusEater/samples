efec034f21ed00e41b237b0a004bf57bac87ee76
Adam M Dutko <adam@oktud.com>
2018-06-11 11:51:31 -0400

    There are a few bugs we need to resolve but the basic histogram works.

---
+++ C/1.13.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_WORD 44
+
+void main() {
+    // Limit our lengths to 45 as that is 'technically' the longest word in 
+    // any major English dictionary per the always 'truthy' Wikipedia... :-)
+    int histogram[MAX_WORD];
+    int index, character, word_size, hash_count = 0;
+
+    // Start using our new formatting approach, much nicer!
+    //
+    // Oh yeah and zero fill our array.
+    for ( index = 0; index <= 44; index++ )
+        histogram[index] = 0;
+
+    while( ( character = getchar() ) != EOF ) {
+	// If the character is a-z or A-Z we consider it part of a readable
+	// word. Maybe this slightly flawed as numbers might be relevant as 
+	// well as special characters like hypens and etc. but this is just a 
+	// simple program at the moment.
+        if ( character != ' ' ) {
+	    word_size++;
+	} else if ( character == ' ' ) {
+	    // If the character is not one of our 'readable' ones then reset 
+            // state!
+	    histogram[word_size]++;
+	    word_size = 0;
+	}
+    }
+
+    for( index = 0; index <= 44; index++ ) {
+	printf("%d: ", index);
+	for( hash_count = 0; hash_count < histogram[index]; hash_count++)
+	    putchar('#');
+	printf("\n");
+	hash_count = 0;
+    }
+
+}
