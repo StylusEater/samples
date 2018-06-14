0cc1bf3dd9d002c5a2349d3259385de2ac6eacbb
Adam M Dutko <adam@oktud.com>
2018-06-14 12:45:13 -0400

    Fixed bugs. Works now.

--- C/1.17.c
+++ C/1.17.c
@@ -1,40 +1,44 @@
 #include <stdio.h>
 #include <signal.h>
+#include <string.h>
 
 /* print all input lines greater than 80 characters */
 
 /* Assume input lines are not longer than 1000 characters. */
 #define LINE_SIZE 1000 
 
+/* zero empty our character array */
 void reset_line_array(char *line) {
-    int i = 0;
-    for( i; i < LINE_SIZE; i++)
-        line[i] = ' ';
-        if ( i == LINE_SIZE - 1 )
-	    line[LINE_SIZE - 1] = '\0';
+    memset(line,0,LINE_SIZE * sizeof(*line));
 }
 
+/* print the contents of the line character array */
 void print_line_array(char *line) {
     int i = 0;
-    printf("\n");
-    putchar('a');
     for( i; i < LINE_SIZE; i++)
         putchar( line[i] );
-    putchar('b');
     reset_line_array(line);
 }
 
 void main()
 {
 
-    int line_length,c  = 0;
+    int line_length = 0;
+    int c  = 0;
     char line[LINE_SIZE];
 
-    while( (c == getchar()) != EOF ) {
+    /* do this so we don't get random garbage in our array */
+    reset_line_array(line);
+
+    while( (c = getchar()) != EOF ) {
         line_length = line_length + 1;
-	if ( c == '\n' ) {
+	if ( c == '\n' && line_length >= 80 ) {
             print_line_array(line); 
 	    line_length = 0;
+	} else if ( c == '\n' && line_length < 80 ) {
+	    reset_line_array(line);
+	    line_length = 0;
+	    printf("Not printing line as it's shorter than 80 characters.\n");
 	} else {
 	    line[line_length] = c;
 	}
