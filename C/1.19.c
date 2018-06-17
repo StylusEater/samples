355a33727aa66b27decdb32f879229236f98a4ae
Adam M Dutko <adam@oktud.com>
2018-06-16 23:24:45 -0400

    Add the 'barebones' for the 1.19 program.

---
+++ C/1.19.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <signal.h>
+#include <string.h>
+
+/* write a function reverse(s) that reverses the charcter string s. Use it to 
+   write a program that reverses its input a line at a time. */
+
+/* Assume input lines are not longer than 1000 characters. */
+#define LINE_SIZE 1000 
+
+/* zero empty our character array */
+void reset_line_array(char *line) {
+    memset(line,0,LINE_SIZE * sizeof(*line));
+}
+
+/* print the contents of the line character array */
+void print_line_array(char *line) {
+    int i = 0;
+    for( i; i < LINE_SIZE; i++)
+        putchar( line[i] );
+    reset_line_array(line);
+}
+
+void main()
+{
+    int line_length = 0;
+    int c  = 0;
+    char line[LINE_SIZE];
+
+    /* do this so we don't get random garbage in our array */
+    reset_line_array(line);
+
+    while( (c = getchar()) != EOF ) {
+	if ( c == ' ' || c == '\t' ) {
+	
+        } else if ( c == '\n' ) {
+            print_line_array(line); 
+	    reset_line_array(line);
+	    line_length = 0;
+	} else {
+	    line_length = line_length + 1;
+	    line[line_length] = c;
+	}
+    }
+}
