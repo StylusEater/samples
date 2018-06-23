fc5491045dd66319c51f5a85e32a099985898d15
Adam M Dutko <adam@oktud.com>
2018-06-22 22:53:26 -0400

    Minor adjustments.

--- C/1.19.c
+++ C/1.19.c
@@ -18,7 +18,6 @@ void print_line_array(char *line) {
     int i = 0;
     for( i; i < LINE_SIZE; i++)
         putchar( line[i] );
-    reset_line_array(line);
 }
 
 /* reverse a line of characters and return the result */
@@ -28,9 +27,19 @@ char* reverse(char *line) {
     char reversed_line[LINE_SIZE];
     char* rl = reversed_line;
     for( index; index < LINE_SIZE - 1; index++) {
-        reversed_line[index] = line[reversed_index];
-        index = index + 1;
-        reversed_index = reversed_index - 1;	
+	if (index == LINE_SIZE - 1) {
+	    reversed_line[index] = '0';
+	} else if (index == LINE_SIZE - 2) {
+	    reversed_line[index] = '\\';
+	} else {
+            reversed_line[index] = line[reversed_index];
+        }
+	if (index < LINE_SIZE) {
+            index = index + 1;
+	}
+	if (reversed_index >= 0) {
+            reversed_index = reversed_index - 1;	
+        }
     } 
 
     return rl;
@@ -41,15 +50,18 @@ void main()
     int line_length = 0;
     int c  = 0;
     char line[LINE_SIZE];
+    char* _rl;
 
     /* do this so we don't get random garbage in our array */
     reset_line_array(line);
 
     while( (c = getchar()) != EOF ) {
 	if ( c == ' ' || c == '\t' ) {
-	
+            // Do nothing as we do not have a new line yet...
         } else if ( c == '\n' ) {
-            print_line_array(line); 
+            _rl = reverse(line);
+            print_line_array(_rl);	    
+            reset_line_array(_rl);
 	    reset_line_array(line);
 	    line_length = 0;
 	} else {
